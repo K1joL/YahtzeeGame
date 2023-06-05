792e4a3fe68293be4c9339146d146cc2c0cac526
K1joL <kzk19@mail.ru>
2023-06-05 20:28:24 +0300

    Create player header

---
+++ Player.h
@@ -0,0 +1,37 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <vector>
+
+class Player
+{
+	class Dice
+	{
+		int m_top = 1;
+		bool m_isKeeped = 0;
+	public:
+		Dice(int top) : m_top{top}
+		{
+		}
+		~Dice() {}
+		int getTop() const { return m_top; }
+		bool isKeeped() const { return m_isKeeped; }
+		void keep() { m_isKeeped = true; }
+	};
+
+private:
+	std::vector<Dice> m_dices;
+
+public:
+	Player()
+	{
+		for (int i = 0; i < 5; i++)
+			m_dices.push_back( Dice{ i + 1 } );
+	}
+	~Player()
+	{
+	}
+	const std::vector<Dice>& getDices() { return m_dices; }
+};
+
+#endif //PLAYER_H
