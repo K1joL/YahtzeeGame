76bf8c26af3d26c56c9f5a50a3544c0fa712299c
K1joL <kzk19@mail.ru>
2023-06-20 13:58:56 +0300

    Added interface to restart game

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include "Game.h"
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
 	Game g;
-	
+
+	while (true)
+	{
+		g.startGame();
+
+		cout << "Want to start new game?	(Y)es or (N)o" << endl;
+		char answer;
+		cin >> answer;
+		if (answer == 'N')
+			break;
+	}
+
 
 	return 0;
 }
